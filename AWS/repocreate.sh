115408cd3c56fcb51779982b60577a15b78748d5
rangapv <rangapv@yahoo.com>
2025-08-03 21:11:11 +0530

    Successfully created Role&Policy in CLI

--- AWS/repocreate.sh
+++ AWS/repocreate.sh
@@ -30,16 +30,38 @@ fi
 
 }
 
+rolecreate() {
+ echo "Input the TRUST policy json file name"
+ read trust1
+ echo "input the PERMISSSION policy json file name"
+ read permin1
+ role1=`aws iam create-role  --role-name ${repo}-role1  --assume-role-policy-document file://${trust1}.json`
+ role1s="$?"
+ if [[ "$role1s" == "0" ]]
+ then
+    echo "role with name ${repo}-role1 created successfully with TRUST permissions"
+    echo "now lets attach the Permission JSON to this Role"
+    permrole1=`aws iam put-role-policy --role-name $[repo}-role1 --policy-document file://${permin1}.json --policy-name simple1`
+    permrole1s="$?"
+    if [[ "$permrole1s" == "0" ]]
+    then
+        echo "SUCCESSFULLY attached Permision policy to the role ${repo}-role1"
+    else
+        echo "FAILED to attach Permission policy to the role ${repo}-role1"
+    fi
+ else
+   echo "Role creation failed"
+ fi
+}
 
 funclambda() {
-
 echo "If you want to create a new Function in command line for Lambda press 'y'"echo "If you want to create a new Function in command line for Lambda press 'y'"
 read lambfunc
 if [[ "${lambfunc}" == "y" ]]
 then
    echo "Enter Name of the Function"
    read funcname
-   echo "Enter the architecture (x86_84/amd64)"
+   echo "Enter the architecture (x86_64/amd64)"
    read lambarch
    echo "Enter Description"
    read lambdesc
@@ -47,16 +69,22 @@ then
    read lambtime
    echo "Enter the package type (Image/Zip)"
    read lambpack
-   lambfunc1=`aws lambda create-function --function-name ${funcname} --architectures ${lambarch} --description {lambdesc} --timeout ${lambtime} --role arn:aws:iam::639266437671:role/service-role/web2-role-8jyz9pf3 --package-type ${lambpack} --code "ImageUri=639266437671.dkr.ecr.us-west-2.amazonaws.com/web1:latest"`
+   lambfunc1=`aws lambda create-function --function-name ${funcname} --architectures ${lambarch} --description {lambdesc} --timeout ${lambtime} --role arn:aws:iam::${account1}:role/${repo}-role1 --package-type ${lambpack} --code "ImageUri=639266437671.dkr.ecr.us-west-2.amazonaws.com/web1:latest"`
    lambfunc1s="$?"
    echo "Trying to create lambda function with name ${funcname}  and architectures ${lambarch} with timeout ${lambtime} of Package type ${lambpack}"
    if [[ "$lambfunc1s" == "0" ]]
    then
        echo "Lambda Function creation SUCCESSFUL ${lambfunc1}"
+       funurl=`aws lambda create-function-url-config --function-name ${funcname} --auth-type NONE`
+       funurls="$?"
+       if [[ "$funurls" == "0" ]]
+       then
+          url1=`echo "$funurl" | grep "FunctionUrl"`
+          echo "Function URL creation is SUCCESS.."
+          echo "You can access your function at ${url1}" 
    else
        echo "Lambda Function creation failed..try again"
    fi
-
 else
    exit
 fi
